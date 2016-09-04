726615fbac641c9fd3e1a122ec9bd133127c957b
Tucker Connelly <web@tuckerconnelly.com>
2016-09-04 09:36:39 -0400

    Update lib folder to match #master

--- src/lib/createNativeComponent.js
+++
@@ -1,7 +0,0 @@
-// Adding this here for webpack. React Native automatically resolves .ios.js and
-// .android.js files, but webpack doesn't
-
-import { Platform } from 'react-native-universal'
-
-if (Platform.OS === 'ios') module.exports = require('./createNativeComponent.ios')
-if (Platform.OS === 'android') module.exports = require('./createNativeComponent.android')
